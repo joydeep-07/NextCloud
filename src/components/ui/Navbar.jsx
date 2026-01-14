74f5949feae32b808ad3ca55e3ee0527647a8394
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-01-14 22:29:07 +0530

    updated

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
             <input
               type="text"
               placeholder="Search files, folders..."
-              className="pl-10 pr-4 py-2.5 w-2xl border border-gray-200 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-gray-50 text-sm transition-all duration-200"
+              className="pl-10 pr-4 py-2.5 w-2xl border border-gray-200 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500/50 focus:border-transparent bg-gray-50 text-sm transition-all duration-200"
             />
           </div>
 
