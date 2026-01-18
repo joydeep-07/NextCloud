26c87b804f1f795a6f9e5c03f40fcca5a7fc2b90
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-01-18 10:35:39 +0530

    updated

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
 
     try {
       await login({ email, password });
-      navigate("/");
+      navigate("/dashboard");
     } catch (err) {
       setError(err.message);
     } finally {
